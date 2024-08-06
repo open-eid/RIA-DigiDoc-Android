ad8d396a2b3ea136584f68bc79f11358db153f43
Boriss Melikjan <boriss.melikjan@nortal.com>
2024-08-06 15:59:06 +0300

    Review dependencies and licenses for Info view.

--- config-lib/build.gradle.kts
+++ config-lib/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.work.runtime.ktx)
     implementation(libs.google.dagger.hilt.android)
     kapt(libs.google.dagger.hilt.android.compile)
     implementation(libs.androidx.hilt)
