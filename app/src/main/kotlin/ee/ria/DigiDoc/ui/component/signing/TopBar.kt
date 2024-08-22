5766170ba08a9ebc2798623e4c00d3fbc4544a21
Boriss Melikjan <boriss.melikjan@nortal.com>
2024-08-22 19:05:11 +0300

    Accessibility for Info View and functionality (Top Bar fix: heading first then back button).
    * MOPPAND-1369 Accessibility for Info View and functionality (Top Bar fix: heading first then back button).
    * MOPPAND-1369 Optimize imports (project wise).

--- app/src/main/kotlin/ee/ria/DigiDoc/ui/component/signing/TopBar.kt
+++ app/src/main/kotlin/ee/ria/DigiDoc/ui/component/signing/TopBar.kt
@@ -3,6 +3,7 @@
 package ee.ria.DigiDoc.ui.component.signing
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.focusable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -13,13 +14,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import ee.ria.DigiDoc.R
 import ee.ria.DigiDoc.ui.component.shared.PreventResize
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +35,22 @@ fun TopBar(
     @StringRes title: Int,
     onBackButtonClick: () -> Unit = {},
 ) {
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(Unit) {
+        delay(200)
+        focusManager.clearFocus()
+        delay(200)
+        focusRequester.requestFocus()
+    }
+
     CenterAlignedTopAppBar(
+        modifier =
+            modifier
+                .semantics {
+                    isTraversalGroup = true
+                },
         colors =
             TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primary,
@@ -40,11 +62,12 @@ fun TopBar(
                     text = stringResource(id = title),
                     maxLines = 1,
                     modifier =
-                        modifier
-                            .semantics { heading() }
-                            .focusProperties {
-                                canFocus = false
-                            },
+                        Modifier
+                            .semantics {
+                                heading()
+                            }
+                            .focusRequester(focusRequester)
+                            .focusable(),
                 )
             }
         },
