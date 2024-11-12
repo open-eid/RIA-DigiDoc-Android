00c7a98ef349883b76daac0f6563d09023f82f5c
Boriss Melikjan <boriss.melikjan@nortal.com>
2024-11-12 15:27:12 +0200

    MOPPAND-1402 Add testTag for Signing Bottom Bar and child buttons.

--- app/src/main/kotlin/ee/ria/DigiDoc/ui/component/signing/SigningBottomBar.kt
+++ app/src/main/kotlin/ee/ria/DigiDoc/ui/component/signing/SigningBottomBar.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.tooling.preview.Preview
 import ee.ria.DigiDoc.ui.component.shared.PrimaryButton
 import ee.ria.DigiDoc.ui.theme.Dimensions.screenViewLargePadding
 import ee.ria.DigiDoc.ui.theme.RIADigiDocTheme
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SigningBottomBar(
     modifier: Modifier,
@@ -24,7 +29,13 @@ fun SigningBottomBar(
     onShareClick: () -> Unit = {},
 ) {
     Column(
-        modifier = modifier.padding(screenViewLargePadding),
+        modifier =
+            modifier
+                .semantics {
+                    testTagsAsResourceId = true
+                }
+                .padding(screenViewLargePadding)
+                .testTag("signingBottomBar"),
     ) {
         signingBottomNavigationItems(
             showSignButton = showSignButton,
@@ -35,8 +46,9 @@ fun SigningBottomBar(
             onShareClick = onShareClick,
         ).forEachIndexed { _, navigationItem ->
             if (navigationItem.showButton) {
-                Row {
+                Row(modifier = modifier) {
                     PrimaryButton(
+                        modifier = modifier.testTag(navigationItem.testTag),
                         title = navigationItem.label,
                         contentDescription = navigationItem.contentDescription,
                         isSubButton = navigationItem.isSubButton,
