938a65722d0c55daf122f08a19a11b72e89d1e06
Boriss Melikjan <boriss.melikjan@nortal.com>
2025-03-14 20:13:00 +0200

    Libcdoc initial integration, Gradle update plugin and task. (#164)
    MOPPAND-1529 Libcdoc initial integration, Gradle update plugin and task.

--- crypto-lib/build.gradle.kts
+++ crypto-lib/build.gradle.kts
@@ -1,24 +1,55 @@
+import ee.ria.DigiDoc.libcdoc.update.LibcdocPlugin
+
+val appAbiFilters = "arm64-v8a;armeabi-v7a;x86_64"
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
+apply<LibcdocPlugin>()
+
 android {
-    namespace = "ee.ria.DigiDoc.crypto"
+    namespace = "ee.ria.DigiDoc.cryptolib"
     compileSdk = Integer.parseInt(libs.versions.compileSdkVersion.get())
 
     defaultConfig {
+        testInstrumentationRunnerArguments += mapOf("clearPackageData" to "true")
         minSdk = Integer.parseInt(libs.versions.minSdkVersion.get())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters.clear()
+            abiFilters.addAll(appAbiFilters.split(';').map { it.trim() })
+        }
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        debug {
+            enableUnitTestCoverage = true
+            enableAndroidTestCoverage = true
+        }
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+        }
     }
 }
 
