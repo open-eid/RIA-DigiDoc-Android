946f79505340fd5534764c7a3d4c57e38cef4116
Sander.Kondratjev <sander.kondratjev@nortal.com>
2025-07-25 14:17:22 +0300

    NFC-50 Add minimal implementation of WebEidViewModel component

---
+++ app/src/main/kotlin/ee/ria/DigiDoc/viewmodel/WebEidViewModel.kt
@@ -0,0 +1,14 @@
+@file:Suppress("PackageName")
+
+package ee.ria.DigiDoc.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WebEidViewModel
+@Inject
+constructor() : ViewModel() {
+
+}
