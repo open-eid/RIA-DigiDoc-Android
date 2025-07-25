1b6f57ae3d358b3d32410b7c8ca22527ac136be1
Sander.Kondratjev <sander.kondratjev@nortal.com>
2025-07-25 16:11:11 +0300

    NFC-50 Display parsed Web EID auth payload in WebEidScreen after URI handling

--- app/src/main/kotlin/ee/ria/DigiDoc/RIADigiDocAppNavigation.kt
+++ app/src/main/kotlin/ee/ria/DigiDoc/RIADigiDocAppNavigation.kt
@@ -363,6 +363,7 @@ fun RIADigiDocAppScreen(externalFileUris: List<Uri>, webEidUri: Uri? = null) {
             WebEidFragment(
                 modifier = Modifier.safeDrawingPadding(),
                 navController = navController,
+                webEidUri = webEidUri,
             )
         }
     }
