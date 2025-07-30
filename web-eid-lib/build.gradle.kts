8963e347b135a3181d5bbac1fed34fa7489610e3
Sander.Kondratjev <sander.kondratjev@nortal.com>
2025-07-30 10:06:00 +0300

    NFC-51 Align web-eid-lib Gradle config with mobile-id-lib settings

--- web-eid-lib/build.gradle.kts
+++ web-eid-lib/build.gradle.kts
@@ -1,46 +1,64 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.example.web_eid_lib"
-    compileSdk = 36
+    compileSdk = Integer.parseInt(libs.versions.compileSdkVersion.get())
 
     defaultConfig {
-        applicationId = "com.example.web_eid_lib"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
+        minSdk = Integer.parseInt(libs.versions.minSdkVersion.get())
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+
+    packaging {
+        resources {
+            pickFirsts += "META-INF/LICENSE.md"
+            pickFirsts += "META-INF/LICENSE-notice.md"
+            pickFirsts += "/META-INF/{AL2.0,LGPL2.1}"
+            pickFirsts += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+        }
+    }
+
+    buildTypes {
+        debug {
+            enableUnitTestCoverage = true
+            enableAndroidTestCoverage = true
+        }
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.gson)
+    implementation(libs.preferencex)
+
+    implementation(libs.google.dagger.hilt.android)
+    kapt(libs.google.dagger.hilt.android.compile)
+    implementation(libs.androidx.hilt)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(project(":libdigidoc-lib"))
+    implementation(project(":networking-lib"))
+    implementation(project(":utils-lib"))
+    implementation(project(":commons-lib"))
+    implementation(project(":config-lib"))
+
+    androidTestImplementation(project(":commons-lib:test-files"))
 }
