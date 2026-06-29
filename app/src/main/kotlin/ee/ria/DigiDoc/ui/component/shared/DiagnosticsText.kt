318bbd254ba2a654b9f56173faa423fd451be99d
Marten Rebane <marten.rebane@nortal.com>
2026-06-30 02:58:52 +0300

    Move central configuration info higher

--- app/src/main/kotlin/ee/ria/DigiDoc/ui/component/shared/DiagnosticsText.kt
+++ app/src/main/kotlin/ee/ria/DigiDoc/ui/component/shared/DiagnosticsText.kt
@@ -21,37 +21,47 @@
 
 package ee.ria.DigiDoc.ui.component.shared
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import ee.ria.DigiDoc.ui.theme.Dimensions.screenViewLargePadding
 import ee.ria.DigiDoc.ui.theme.Dimensions.screenViewSmallPadding
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun DiagnosticsText(
-    modifier: Modifier,
-    text1: String,
-    text2: String,
+    modifier: Modifier = Modifier,
+    testTag: String,
+    @StringRes labelRes: Int,
+    value: String,
     textAlign: TextAlign = TextAlign.Start,
 ) {
+    val label = stringResource(id = labelRes)
     val annotatedString =
         buildAnnotatedString {
-            append(text1)
-            append(text2)
+            append(label)
+            append(value)
         }
 
     Text(
         modifier =
             modifier
                 .semantics {
-                    contentDescription = text1.lowercase() + " " + text2.lowercase()
-                }.padding(
+                    testTagsAsResourceId = true
+                    contentDescription = label.lowercase() + " " + value.lowercase()
+                }.testTag(testTag)
+                .padding(
                     start = screenViewLargePadding,
                     top = screenViewSmallPadding,
                     end = screenViewLargePadding,
